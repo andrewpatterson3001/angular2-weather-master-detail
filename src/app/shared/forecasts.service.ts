a1fee38fe1ba1a78d1cf606e38c55d84ed82c476
Andrew Patterson <andrewpatterson@MacBook-Air-3001.local>
2017-04-13 23:10:11 -0700

    add Dark Sky Api key without exposing to Github

--- src/app/shared/forecasts.service.ts
+++ src/app/shared/forecasts.service.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@angular/core';
 import { Forecast } from './forecast.model';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
-import { darkSkyKey } from 'src/config.json'
-const BASE_URL = 'https://api.darksky.net/forecast/#{DARKSKYKEY}/34.416918,-119.831111';
+import { darkSkyKey } from './config'
+const BASE_URL = "https://api.darksky.net/forecast/" + darkSkyKey + "/34.416918,-119.831111";
 const HEADER = { headers: new Headers({ 'Content-Type': 'application/json' }) };
 
 @Injectable()
